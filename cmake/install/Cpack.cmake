934026fe1d1ae5f895ef335bee4d8b7e870b4960
msclock <msclock@qq.com>
2023-10-23 03:03:25 +0800

    feat: add a tool to configure cpack
    Signed-off-by: msclock <msclock@qq.com>

---
+++ cmake/install/Cpack.cmake
@@ -0,0 +1,6 @@
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
+set(CPACK_SOURCE_IGNORE_FILES /.git /dist /.*build.* /\\\\.DS_Store)
+include(CPack)
+# https://github.com/MangaD/cpp-project-template/blob/main/cmake/cpack_module.cmake
