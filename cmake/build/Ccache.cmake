772f0fa899a43d252bd49c1dacb7d6eee8138e26
msclock <msclock@qq.com>
2023-10-23 02:20:32 +0800

    perf: change ccache verify flow

--- cmake/build/Ccache.cmake
+++ cmake/build/Ccache.cmake
@@ -1,22 +1,36 @@
 #[[
-Configure ccache optimization for compiling cache
+Configure ccache optimization for compiling.
 
 Example:
 
   include(Ccache)
 
 ]]
-macro(ccache_enable)
-  find_program(CCACHE_PROGRAM ccache)
-  if(CCACHE_PROGRAM)
-    message(STATUS "Enable ccache")
-    set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
-    set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
-    # set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
-    # set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
-  else()
-    message(STATUS "Disable ccache because of no ccache installed")
-  endif()
-endmacro()
 
-ccache_enable()
+include_guard(GLOBAL)
+
+set(USE_CCACHE
+    ON
+    CACHE BOOL "use ccache to speed up compiling.")
+
+if(NOT USE_CCACHE)
+  message(STATUS "Disable ccache")
+  return()
+endif()
+
+find_program(
+  CCACHE_COMMAND
+  NAMES ccache
+  DOC "ccache executable")
+
+if(CCACHE_COMMAND)
+  message(STATUS "Activate ccache: ${CCACHE_COMMAND}")
+  set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_COMMAND}")
+  set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_COMMAND}")
+
+  # set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+
+  # set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+else()
+  message(WARNING "Disable ccache because of no ccache installed")
+endif()
