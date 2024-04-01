d1585c411bf7d4be4aec9d46475a2a094e73659e
msclock <msclock@qq.com>
2024-04-02 01:12:56 +0800

    fix: harden_target is missing (#73)
    Signed-off-by: msclock <msclock@qq.com>

--- cmake/build/Hardening.cmake
+++ cmake/build/Hardening.cmake
@@ -86,7 +86,6 @@ message(
 
 if(NOT USE_HARDENING)
   message(STATUS "Hardening disabled by USE_HARDENING evaluates to false")
-  return()
 endif()
 
 message(VERBOSE "Check Hardening flags: ${USE_HARDENING_FLAGS}")
