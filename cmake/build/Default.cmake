b7560d4f6d3adbde4b1a50371cd70b1237624379
msclock <msclock@qq.com>
2024-05-21 21:44:34 +0800

    perf: cache Default modules (#109)
    Signed-off-by: msclock <msclock@qq.com>

--- cmake/build/Default.cmake
+++ cmake/build/Default.cmake
@@ -3,14 +3,18 @@ Default to build.
 ]]
 include_guard(GLOBAL)
 
-# Create the compile command database for clang by default
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(CMAKE_EXPORT_COMPILE_COMMANDS
+    ON
+    CACHE BOOL "Generate compile_commands.json")
 
-# Always build with -fPIC
-set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+set(CMAKE_POSITION_INDEPENDENT_CODE
+    ON
+    CACHE BOOL "Always build position-independent code")
 
-# This will cause all libraries to be built shared unless the library was
-# explicitly added as a static library.  This variable is often added to
-# projects as an ``option()`` so that each user of a project can decide if they
-# want to build the project using shared or static libraries.
-set(BUILD_SHARED_LIBS OFF)
+set(BUILD_SHARED_LIBS
+    OFF
+    CACHE
+      BOOL
+      "This will cause all libraries to be built shared unless the library was explicitly added as a static library.
+This variable is often added to projects as an ``option()`` so that each user of a project can decide if they want
+to build the project using shared or static libraries.")
