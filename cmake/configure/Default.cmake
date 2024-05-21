dd4d18b0a612b69a63a1373dda39227f3c3e33ab
msclock <msclock@qq.com>
2024-05-21 17:56:49 +0800

    perf: cache Default modules
    Signed-off-by: msclock <msclock@qq.com>

--- cmake/configure/Default.cmake
+++ cmake/configure/Default.cmake
@@ -3,10 +3,13 @@ Default to configuration.
 ]]
 include_guard(GLOBAL)
 
-# Put the include dirs which are in the source or build tree before all other
-# include dirs, so the headers in the sources are preferred over the already
-# installed ones since cmake 2.4.1
-set(CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE ON)
+set(CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE
+    ON
+    CACHE
+      BOOL
+      "Put the include dirs which are in the source or build tree before all other include dirs, so the headers in the sources are preferred over the already installed ones since cmake 2.4.1"
+)
 
-# Open FetchContent fetching logs.
-set(FETCHCONTENT_QUIET ON)
+set(FETCHCONTENT_QUIET
+    ON
+    CACHE BOOL "Don't print messages about fetch operations.")
