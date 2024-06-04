fa1679d12d42a77024e35ec97050e28c0e4b5c75
msclock <msclock@qq.com>
2024-06-04 10:33:01 +0800

    add more refs
    Signed-off-by: msclock <msclock@qq.com>

--- cmake/install/Cpack.cmake
+++ cmake/install/Cpack.cmake
@@ -4,3 +4,5 @@ set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
 set(CPACK_SOURCE_IGNORE_FILES /.git /dist /.*build.* /\\\\.DS_Store)
 include(CPack)
 # https://github.com/MangaD/cpp-project-template/blob/main/cmake/cpack_module.cmake
+# https://github.com/gabime/spdlog/blob/v1.x/cmake/spdlogCPack.cmake
+# https://github.com/retifrav/cmake-cpack-example/tree/master
